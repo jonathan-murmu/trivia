404809c747ed5011c94229230e00a431cec32083
jonathanmurmu <jonathan.murmu@mindfiresolutions.com>
2018-04-16 17:34:19 +0530

    corsheader and allowed hosts and jwt settings

--- trivia/config/local.py
+++ trivia/config/local.py
@@ -4,3 +4,11 @@ from .base import *
 
 STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, STATIC_PATH))
 # MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, MEDIA_PATH))
+
+ALLOWED_HOSTS += ['13.127.237.253', 'ec2-13-127-237-253.ap-south-1.compute.amazonaws.com',
+                  'localhost']
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+    '127.0.0.1:4200'
+)
--- trivia/config/staging.py
+++ trivia/config/staging.py
@@ -5,3 +5,8 @@ ALLOWED_HOSTS += ['13.127.237.253', 'ec2-13-127-237-253.ap-south-1.compute.amazo
 
 STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, STATIC_PATH))
 # MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, MEDIA_PATH))
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+    '127.0.0.1:4200'
+)
